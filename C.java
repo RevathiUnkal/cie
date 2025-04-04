e17c6aa47613dbc0be5eeed29e4c89b7537341e9
sumita12345 <sumitamsomankatti@gmail.com>
2025-04-04 05:40:39 +0530

    save all

---
+++ C.java
@@ -0,0 +1,13 @@
+package constructor;
+    public class C extends B{
+        C(){
+           System.out.println("Constructor C");
+       }
+   }
+
+
+   class print {
+    public static void main(String[] args) {
+    }
+}
+    
