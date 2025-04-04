766b74c759b032efb840e7faad23313392f3f864
sumita12345 <sumitamsomankatti@gmail.com>
2025-04-04 05:43:21 +0530

    save all

---
+++ A.java
@@ -0,0 +1,10 @@
+package constructor;
+public class A {
+    A(){
+        System.out.println("constructor A");
+    }}
+
+
+
+
+
---
+++ B.java
@@ -0,0 +1,7 @@
+package constructor;
+
+    public class B extends A{
+        B(){
+            System.out.println("Constructor B");
+        }}
+
